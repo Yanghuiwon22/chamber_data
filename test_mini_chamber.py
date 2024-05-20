1b0ece5319e24f2b8bcd1f3f6877c74ba809f021
Yanghuiwon22 <hannah4212@naver.com>
2024-05-20 11:24:41 +0900

    /

--- test_mini_chamber.py
+++ test_mini_chamber.py
@@ -8,7 +8,7 @@ url = 'https://api.thingspeak.com/channels/1999883/feeds.json?api_key=XP1R5CVUPV
 results = 0 # 받고자 하는 데이터 수 (현재를 기준으로 과거데이터, 데이터간격 3분)
 
 def get_data():
-    response = requests.get(url+'results=1440')
+    response = requests.get(url+'results=5000')
 
     if response.status_code == 200:
         df_chamber = pd.DataFrame(response.json()['feeds'])
@@ -18,29 +18,28 @@ def get_data():
 
         df_chamber.drop(['created_at','field4'], axis=1, inplace=True)
         df_chamber.rename(columns={'field1': 'temp', 'field2': 'hum', 'field3': 'lux'}, inplace=True)
-        df_chamber.to_csv('chamber.csv', index=False)
 
     grouped = df_chamber.groupby('Date')
-
     for group_name, group_df in grouped:
-        print(f"========={group_name}=========")
-        print(group_df)
-        group_df.to_csv(f'./output/{group_name}.csv', index=False)
+        if group_name >= '2024-05-17':
+            print(f"========={group_name}=========")
+            print(group_df)
+            group_df.to_csv(f'./output/{group_name}.csv', index=False)
 
-def draw_graph():
-    df = pd.read_csv('chamber.csv')
+def draw_graph(date):
+    df = pd.read_csv(f'{date}.csv')
 
     ax = sns.lineplot(data=df, x='Time', y='temp')
     ax.set_xticks(range(0, len(df)+1, 60))
-    plt.show()
+    plt.savefig(f'{date}.png')
 
 
 
 def main():
-    print("""
-    =================run main=================""")
+    output_dir = './output'
+
     get_data()
-    # draw_graph()
+    draw_graph(f'{output_dir}/2024-05-20')
 
 if __name__ == '__main__':
     main()
