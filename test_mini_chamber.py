79c11d2235cb6dcb5a960ec99468bb3691482e11
Yanghuiwon22 <hannah4212@naver.com>
2024-08-15 01:24:43 +0900

    중간 점검 -ing (오류시 바로잡는 코드만 작성완료 --> 챔버 켜야함

--- test_mini_chamber.py
+++ test_mini_chamber.py
@@ -10,12 +10,14 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 
 url = 'https://api.thingspeak.com/channels/1999883/feeds.json?api_key=XP1R5CVUPVXTNJT0&'
 results = 0 # 받고자 하는 데이터 수 (현재를 기준으로 과거데이터, 데이터간격 3분)
 
 def get_data():
-    response = requests.get(url+'results=5000')
+    response = requests.get(url+'results=50000')
 
     if response.status_code == 200:
         df_chamber = pd.DataFrame(response.json()['feeds'])
@@ -35,12 +37,14 @@ def get_data():
     grouped = df_chamber.groupby('Date')
     for group_name, group_df in grouped:
         print(len(group_df))
+        print(group_df)
 
         if group_name >= '2024-05-17':
             group_df.to_csv(f'./output/csv/{group_name}.csv', index=False)
             print(f'{group_name} 성공')
         else:
             print(f'{group_name} 실패')
+
 
 def draw_graph(date, y='t_h'):
     df = pd.read_csv(f'output/csv/{date}.csv')
@@ -95,54 +99,42 @@ def draw_lux_graph(date, y='t&h'):
 
 
 def main():
-    # get_data()
+    get_data()
 
     # now = datetime.now()
     # now_date = now.date()
     # draw_lux_graph(now_date, 'lux')
     # draw_graph(now_date)
 
-    get_error_date()
+    ## 오류가 날 경우 실행시킬 코드
+    # error_date = get_error_date()
+    error_date = ['2024-07-15', '2024-07-16', '2024-07-17', '2024-07-18', '2024-07-19', '2024-07-20', '2024-07-21', '2024-07-22',
+     '2024-07-23', '2024-07-24', '2024-07-25', '2024-07-26', '2024-07-27', '2024-07-28', '2024-07-29', '2024-07-30',
+     '2024-07-31', '2024-08-01', '2024-08-02', '2024-08-03', '2024-08-04', '2024-08-05', '2024-08-06', '2024-08-07',
+     '2024-08-08', '2024-08-09', '2024-08-10', '2024-08-11', '2024-08-12', '2024-08-13', '2024-08-14']
+
+    for date in error_date:
+        draw_graph(date)
+        draw_lux_graph(date, 'lux')
 
 
 # 특정일부터 오류가 계속될 경우, 해당 기간에 대한 데이터를 생성 후 저장하는 함수
 def get_error_date():
+    print('loading get_errordate(01/08)')
     base_url = 'https://github.com/Yanghuiwon22/chamber_data/tree/main/output/graph'
 
     response = requests.get(base_url)
     soup = BeautifulSoup(response.text, 'html.parser')
+    print('loading get_errordate(02/08)')
 
-    final_date = soup.find('div').find_all('a', {'class':'Link--primary'})
-    # print(final_date)
+    # selenium
+    op = Options()
+ar    print('loading get_errordate(03/08)')
 
-    files = soup.find_all('a', string=lambda title: 't_h' in title if title else False)
-    # print(files[0].text.split('_'))
-    #
-    # contents = response.json()
-    # date = contents[-1]
-    # print(contents)
-
-    # page_number = 1
-    # last_file_name = 'fail'
-    #
-    # while True:
-    #     now_page = f'{base_url}?page={page_number}'
-    #     now_response = requests.get(now_page)
-    #     soup = BeautifulSoup(now_response.text, 'html.parser')
-    #
-    #     files = soup.find_all('a', {'class':'Link--primary'})
-    #
-    #     if not files:
-    #         break
-    #
-    #     last_file_name = files[-1].text
-    #     page_number += 1
-    #
-    # print(last_file_name)
-
-
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=op)
     driver.get(base_url)
+    print('loading get_errordate(04/08)')
+
 
     while True:
         last_height = driver.execute_script("return document.body.scrollHeight")
@@ -152,27 +144,28 @@ def get_error_date():
         time.sleep(1)
         if new_height == last_height:
             time.sleep(1)
+            print('loading get_errordate(05/08)')
             break
-
-    ## 스크롤 완.
-
 
     time.sleep(1)
     rows = driver.find_elements(By.XPATH, "//tbody/tr")
     rows_len = len(rows)
+    print('loading get_errordate(06/08)')
 
-    print(driver.find_elements(By.XPATH, f'//*[@id="folder-row-{rows_len-2}"]/td[2]/div/div/div/div/a')[0].text)
+    final_date = driver.find_elements(By.XPATH, f'//*[@id="folder-row-{rows_len-2}"]/td[2]/div/div/div/div/a')[0].text.split('_')[0]
+    print('loading get_errordate(07/08)')
+    print('='*20)
 
-    # for row in rows:
-    #     cells = row.find_elements(By.CLASS_NAME, "Link--primary")
-    #     print(len(cells))
-    #
-    #     for cell in cells:
-    #         print(cell.text)
-
-
-
+    # 처리되지 않는 날짜들 구하기
+    final_date = datetime.strptime(final_date, '%Y-%m-%d').date()
     today_date = datetime.date(datetime.now())
+
+    error_date = [(final_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(1, (today_date - final_date).days)]
+    print('loading get_errordate(08/08)')
+
+    return error_date
+
+
 
 
 
